f9b8920f2f2eab8938e90c0ca0604515f3027504
Axel Chacón Montero <axelchaconmontero8@gmail.com>
2023-01-10 21:01:22 -0600

    variables 1

---
+++ Curso JS Moderno/02-Variables/js/01-app.js
@@ -0,0 +1,22 @@
+// Inicializar una variable con un valor
+var producto= "Monitor de 24 Pulgadas";
+console.log(producto);
+
+// Las variables se pueden reasignar
+var producto= "Monitor de 19 Pulgadas";
+console.log(producto);
+
+// JavaScript es un lenguaje de tipo dinámico, no se especifica tipo de dato 
+producto= 20;
+console.log(producto);
+
+//Se pueden inicializar sin valor y asignarlo después
+var disponible;
+disponible= true;
+
+disponible=false;
+
+//Inicializar múltiples variables
+var categoría= 'Computadoras',
+    marca= "Marca Famosa",
+    calificación =5;
